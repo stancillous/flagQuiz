41a09e5b53dbcf11076b889a777b2a719feb2073
raymond <stancillousray@gmail.com>
2024-07-16 18:47:53 +0300

    remove comments

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <div class="index-wrp">
-      <!-- <div class="bg-video-wrp">
-        <div class="bg-video">
-          <video src="../assets/countries.mp4" autoplay loop muted></video>
-        </div>
-      </div> -->
       <div class="index-wrp-content">
         <component :is="currentComponent"></component>
         <!-- <KeepAlive> </KeepAlive> -->
@@ -46,31 +41,7 @@ const currentComponent = computed(() => {
   display: grid;
   place-items: center;
   position: relative;
-  // .bg-video-wrp {
-  //   position: absolute;
-  //   top: 0;
-  //   left: 0;
-  //   right: 0;
-  //   bottom: 0;
-  //   height: 100%;
-  //   width: 100%;
-  //   z-index: -1;
-  //   .bg-video {
-  //     width: 100%;
-  //     height: 100%;
-  //     border: 5px solid blue;
-  //     video {
-  //       height: 100%;
-  //       width: 100%;
-  //       position: fixed;
-  //       right: 0;
-  //       bottom: 0;
-  //       min-width: 100vw;
-  //       border: 8px solid green;
-  //       min-height: 100vh;
-  //     }
-  //   }
-  // }
+
   .index-wrp-content {
     background-color: white;
     padding: 1rem 0;
